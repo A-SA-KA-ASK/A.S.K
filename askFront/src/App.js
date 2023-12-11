2984c08c4320be64ccf702068491c125c72a170b
Hongaproject <tkwlscjq99@gmail.com>
2023-12-11 22:26:40 +0900

    프론트 서버 폴더 설정

---
+++ askFront/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+
+  // Tailwind CSS / react-icons / react-router-dom 으로 초기 진행 예정
+
+  return (
+    <div className="text-blue-500 text-xl font-bold">
+      초기 페이지 생성
+      
+    </div>
+  );
+}
+
+export default App;
