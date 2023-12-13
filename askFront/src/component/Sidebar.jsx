515c827b3258cded87037b50026f469d9963a83c
Hongaproject <tkwlscjq99@gmail.com>
2023-12-14 01:28:37 +0900

    수정: 사이드바 틀 및 내용 부분 테이블 제작[MP]

--- askFront/src/component/Sidebar.jsx
+++ askFront/src/component/Sidebar.jsx
@@ -1,33 +1,39 @@
+
+
 function Sidebar () {
 
     return(
-        <div className="bg-slate-600 w-48 h-96 float-left">
-            사이드 바 전체 부분 입니다.
-            <div className="border-2 border-slate-950">
-                로그인 틀
-                <div className="bg-slate-500 ">
-                    로그인 부분 입니다.
+        <div className=" w-1/6 h-96 float-left m-7 p-3">
+            <div>
+                <div className="border-2 rounded-2xl text-center p-4 mb-4">
+                    로그인
                 </div>
-            </div>
-            <div className="border-2 border-slate-950">
-                글쓰기 버튼 틀
-                <div className="bg-slate-400">
-                    글쓰기 부분입니다.
+                <div className="border-2 rounded-2xl text-center p-4 mb-4">
+                    글쓰기
                 </div>
             </div>
 
-            <div className="bg-slate-300 border-t-8 border-slate-50">
-                카테고리 틀 입니다.
-                <div className="border-2 border-slate-950">
-                    카테고리 일상 틀 입니다.
-                    <div className="bg-slate-400">
-                        카테고리 일상 txt부분 입니다.
+            <div className="border-t-8 border-r-slate-500">
+                <div className="mt-4 mb-4">
+                    <div className="border-b border-slate-300 py-3 font-bold">
+                        일상
+                    </div>
+                    <div className="py-1">
+                        txt 일상
+                    </div>
+                    <div className="">
+                        이미지 일상 
                     </div>
                 </div>
-                <div className="border-2 border-slate-950">
-                    카테고리 공부 틀 입니다.
-                    <div className="bg-slate-400">
-                        카테고리 공부 txt부분 입니다.
+                <div>
+                <div className="border-b border-slate-300 py-3 font-bold">
+                        공부
+                    </div>
+                    <div className="py-1">
+                        txt 공부
+                    </div>
+                    <div className="">
+                        이미지 공부  
                     </div>
                 </div>
             </div>
--- askFront/src/layout/Section.jsx
+++ askFront/src/layout/Section.jsx
@@ -2,9 +2,13 @@ import Content from "../component/Content";
 import Sidebar from "../component/Sidebar";
 
 function Section() {
+
+    // useState와 useEffect를 사용해서 Content부분에 테이블 내용을 보여주게 만들 예정
+
+    // Section부분에서 component폴더에 있는 컴포넌트들을 보여줌.
+
     return(
-        <div className="h-screen w-11/12  m-auto">
-            <h1>섹션입니다.</h1>
+        <div className="h-screen w-11/12 m-auto mt-6 pt-6">
             <Sidebar />
             <Content />
         </div>
